ad9e24ecfa6f6860c66fb193f3cbc9e0cb4819fb
Rishabh Johri <rishabh2000johri2@gmail.com>
2025-02-08 15:44:19 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from motor.motor_asyncio import AsyncIOMotorClient
 
 #MONGO_URI = "mongodb://datacenter:27017"
 #MONGO_URI = "mongodb://localhost:27017"   
-MONGO_URI = "mongodb://mongodb:27017"
-
+#MONGO_URI = "mongodb://mongodb:27017"
+MONGO_URI = "mongodb://admin:adminpass@192.168.56.107:27017/auth_service?authSource=admin"
 DB_NAME = "auth_service"
 
 client = AsyncIOMotorClient(MONGO_URI)
